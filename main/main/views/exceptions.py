d70adf53db1e2693a1df9135af086a9068ccf0bc
Tom <kudu92@gmail.com>
2019-12-05 23:13:37 +0100

    add code number into http exception log

--- main/main/views/exceptions.py
+++ main/main/views/exceptions.py
@@ -7,12 +7,12 @@ from main.utils import logs
 @exception_view_config(Exception)
 def exception_view(exc, request):
     log = logs.ice_log(__name__, request)
-    log.warn('[EXCEPTION][NAME {}] {}'.format(type(exc).__name__, exc))
+    log.warn('[EXCEPTION] [NAME {}] {}'.format(type(exc).__name__, exc))
     capture_exception(exc)
     return Response(status_int = 500)
 
 @exception_view_config(http_exc.HTTPException)
 def http_exception_view(exc, request):
     log = logs.ice_log(__name__, request)
-    log.info('[HTTP_EXCEPTION][NAME {}]'.format(type(exc).__name__))
+    log.info('[HTTP_EXCEPTION] [NAME {}] [CODE {}]'.format(type(exc).__name__, exc.code))
     return Response(status_int = exc.code)
