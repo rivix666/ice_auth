df86225cd7fce6435c156fd46bda6a8deb51470f
Tom <kudu92@gmail.com>
2019-12-05 23:10:02 +0100

    add http exceptions handle

--- main/main/views/exceptions.py
+++ main/main/views/exceptions.py
@@ -1,17 +1,18 @@
 from pyramid.response import Response
 from pyramid.view import exception_view_config
-import pyramid.httpexceptions as exceptions
+import pyramid.httpexceptions as http_exc
 from sentry_sdk import capture_exception
 from main.utils import logs
 
 @exception_view_config(Exception)
 def exception_view(exc, request):
-    if isinstance(exc, Exception):
-        log = logs.ice_log(__name__, request)
-        log.warn('[EXCEPTION][NAME {}] {}'.format(type(exc).__name__, exc))
-        if isinstance(exc, exceptions.HTTPException):
-            return Response(status_int = exc.code)
-        else:
-            capture_exception(exc)
-
+    log = logs.ice_log(__name__, request)
+    log.warn('[EXCEPTION][NAME {}] {}'.format(type(exc).__name__, exc))
+    capture_exception(exc)
     return Response(status_int = 500)
+
+@exception_view_config(http_exc.HTTPException)
+def http_exception_view(exc, request):
+    log = logs.ice_log(__name__, request)
+    log.info('[HTTP_EXCEPTION][NAME {}]'.format(type(exc).__name__))
+    return Response(status_int = exc.code)
